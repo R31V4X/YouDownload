a999fee3324791c14cd78b3033add2085194324a
R31V4X <johnson.gd007@gmail.com>
2022-10-13 16:54:14 -0400

    initial commit

---
+++ tests/test.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter.ttk import *
+
+window = Tk()
+window.geometry("300x200")
+
+label  = Label(window, text="Downloading.")
+label.pack()
+
+def loop(i=0):
+    label["text"] = "Downloading." + str((i % 3) * ".")
+    window.after(400, loop, i+1)
+loop()
+
+window.mainloop()
