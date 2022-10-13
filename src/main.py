7012e4d938bbcb3a308ccf6b5f060780783f881a
R31V4X <johnson.gd007@gmail.com>
2022-10-13 18:05:10 -0400

    window set up

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
 from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+
+WIN_WIDTH, WIN_HEIGHT = 500, 400
+#SCREEN_WIDTH, SCREEN_HEIGHT = root.winfo_screenwidth(), root.winfo_screenheight()
+WIN_X, WIN_Y = int(root.winfo_screenwidth()/2 - WIN_WIDTH/2), int(root.winfo_screenheight()/2 - WIN_HEIGHT/2)
+
+root.title("YouDownload")
+root.iconbitmap("src/icon.ico")
+root.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}+{WIN_X}+{WIN_Y}")
+
+
+
+
+root.mainloop()
 
